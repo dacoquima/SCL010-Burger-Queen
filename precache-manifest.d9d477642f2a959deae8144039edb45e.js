5827740fdf0bc4b3f6a24c2b43d2d06910f629c1
dacoquima <danimalistica@gmail.com>
2019-10-07 17:56:55 -0300

    Updates

---
+++ precache-manifest.d9d477642f2a959deae8144039edb45e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f121db1e0dd84283cdda64dbbdaada0d",
+    "url": "/SCL010-Burger-Queen/index.html"
+  },
+  {
+    "revision": "01ad959364ac2081a451",
+    "url": "/SCL010-Burger-Queen/static/css/2.cb0a7b97.chunk.css"
+  },
+  {
+    "revision": "b7ce23df4c9cb5158d56",
+    "url": "/SCL010-Burger-Queen/static/css/main.20ce99ca.chunk.css"
+  },
+  {
+    "revision": "01ad959364ac2081a451",
+    "url": "/SCL010-Burger-Queen/static/js/2.0385da05.chunk.js"
+  },
+  {
+    "revision": "b7ce23df4c9cb5158d56",
+    "url": "/SCL010-Burger-Queen/static/js/main.01bb3de3.chunk.js"
+  },
+  {
+    "revision": "3a821617a4d4dba1cad2",
+    "url": "/SCL010-Burger-Queen/static/js/runtime~main.35e6da41.js"
+  },
+  {
+    "revision": "8a300f700382461962dc91e01a63c8cf",
+    "url": "/SCL010-Burger-Queen/static/media/bg-image@2x.8a300f70.png"
+  },
+  {
+    "revision": "5d9005735e8efd7059e9e949947b351d",
+    "url": "/SCL010-Burger-Queen/static/media/big-burger@2x.5d900573.png"
+  }
+]);
